5f1f5db4dcff40a105a0e8bea0065eac611b7e39
Vasanth kumar Kalaiselvan <itsparser@gmail.com>
2024-09-24 20:51:55 +0530

    reformated the src

--- src/langval/eval/langchain.py
+++ src/langval/eval/langchain.py
@@ -2,9 +2,7 @@ import logging
 from typing import Any
 
 from langchain_core.language_models import BaseChatModel
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
-from langgraph.prebuilt import create_react_agent
-from pydantic import BaseModel
+from langchain_core.prompts import PromptTemplate
 
 from langval.error import EvalThreshold
 from langval.eval.base import BaseEval
@@ -13,51 +11,53 @@ from langval.prompt import LANGCHAIN_SYSTEM_PROMPT
 
 
 class LangchainEval(BaseEval):
-	"""
-	Langchain evaluation class for evaluating the toxicity, accuracy, hallucination, and bias of language models.
-	"""
+    """
+    Langchain evaluation class for evaluating the toxicity, accuracy, hallucination, and bias of language models.
+    """
 
-	def __init__(self, llm: BaseChatModel, *, validation: Validation = None):
-		super().__init__(validation=validation)
-		self.llm = llm
+    def __init__(self, llm: BaseChatModel, *, validation: Validation = None):
+        super().__init__(validation=validation)
+        self.llm = llm
 
-	@property
-	def node(self):
-		prompt = PromptTemplate.from_template(LANGCHAIN_SYSTEM_PROMPT)
-		llm = prompt | self.llm.with_structured_output(EvalMetric)
-		return llm
+    @property
+    def node(self):
+        prompt = PromptTemplate.from_template(LANGCHAIN_SYSTEM_PROMPT)
+        llm = prompt | self.llm.with_structured_output(EvalMetric)
+        return llm
 
-	def eval(
-		self,
-		answer: Any,
-		question: Any = None,
-		expected_answer: Any = None,
-		validation: Validation = None,
-	) -> dict[str, Any]:
-		"""
-		Evaluates the toxicity, accuracy, hallucination, and bias of a language model.
+    def eval(
+        self,
+        answer: Any,
+        question: Any = None,
+        expected_answer: Any = None,
+        validation: Validation = None,
+    ) -> dict[str, Any]:
+        """
+        Evaluates the toxicity, accuracy, hallucination, and bias of a language model.
 
-		Args:
-		    question (Any): The question to evaluate.
-		    answer (Any): The answer to evaluate.
-		    expected_answer (Any, optional): The expected answer. Defaults to None.
-		    validation (Validation, optional): The validation to evaluate the model. Defaults to None.
+        Args:
+            question (Any): The question to evaluate.
+            answer (Any): The answer to evaluate.
+            expected_answer (Any, optional): The expected answer. Defaults to None.
+            validation (Validation, optional): The validation to evaluate the model. Defaults to None.
 
-		Returns:
-		    EvalMetric: The evaluation metric.
-		"""
-		if not validation:
-			validation = self.validation
-		if question:
-			question = f'question -->\n {question}\n'
-		if expected_answer:
-			expected_answer = f'Expected answer -->\n {expected_answer}\n'
-		validation_result: dict | EvalMetric = self.node.invoke(
-			{'question': question, 'answer': answer, 'expected_answer': expected_answer}
-		)
-		result, exact_match = validation.compare(validation_result)
-		if exact_match:
-			logging.warning(f'Following exact match found to be in Meet Expectation: {exact_match}')
-		if result:
-			raise EvalThreshold(result)
-		return {'score': validation_result, 'result': result}
+        Returns:
+            EvalMetric: The evaluation metric.
+        """
+        if not validation:
+            validation = self.validation
+        if question:
+            question = f"question -->\n {question}\n"
+        if expected_answer:
+            expected_answer = f"Expected answer -->\n {expected_answer}\n"
+        validation_result: dict | EvalMetric = self.node.invoke(
+            {"question": question, "answer": answer, "expected_answer": expected_answer}
+        )
+        result, exact_match = validation.compare(validation_result)
+        if exact_match:
+            logging.warning(
+                f"Following exact match found to be in Meet Expectation: {exact_match}"
+            )
+        if result:
+            raise EvalThreshold(result)
+        return {"score": validation_result, "result": result}
--- tests/test_eval.py
+++ tests/test_eval.py
@@ -5,15 +5,15 @@ from langchain_openai import ChatOpenAI
 from langval.eval.langchain import LangchainEval
 from langval.model import Validation
 
-llm = ChatOpenAI(model='gpt-4o-mini', temperature=0.3)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.3)
 _eval = LangchainEval(
-	llm, validation=Validation(toxicity=0.2, accuracy=0.9, hallucination=0.2, bias=0.1)
+    llm, validation=Validation(toxicity=0.2, accuracy=0.9, hallucination=0.2, bias=0.1)
 )
 
 
 class TestEval(TestCase):
-	model = llm
+    model = llm
 
-	@_eval.question('What is the capital of France?')
-	def test_eval(self):
-		return 'paris'
+    @_eval.question("What is the capital of France?")
+    def test_eval(self):
+        return "paris"
