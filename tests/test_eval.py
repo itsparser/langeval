95fbf9aa47f1521f0d38f16901f0c2b40091d44c
Vasanth kumar Kalaiselvan <itsparser@gmail.com>
2024-09-25 23:20:33 +0530

    fixed the testcase

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -7,7 +7,7 @@ from langval.eval.langchain import LangchainEval
 from langval.model import Validation
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.3)
-_eval = LangchainEval(
+eval = LangchainEval(
     llm, validation=Validation(toxicity=0.2, accuracy=0.9, hallucination=0.2, bias=0.1)
 )
 
@@ -15,7 +15,7 @@ _eval = LangchainEval(
 class TestEval(TestCase):
     @property
     def evaluator(self) -> BaseEval:
-        return _eval
+        return eval
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -23,12 +23,12 @@ class TestEval(TestCase):
         super().setUpClass()
 
     @langval.assess(model=llm, question="What is the capital of France?")
-    def test_eval(self):
+    def test_case_001(self):
         return "paris"
 
     @langval.assess("What is the capital of India?")
-    def test_001(self):
-        return "paris"
+    def test_case_002(self):
+        return "new delhi"
 
     @classmethod
     def tearDownClass(cls):
